e07f95892cb5b7c535c57150696cb2147153fe7b
TechHiveDevs <techhivedevs@gmail.com>
2022-11-08 15:22:04 +0000

    inital auto commit

---
+++ apps/client/entities/entites.ts
@@ -0,0 +1,8 @@
+
+import User from "./user/ui.user";
+import Pots from "./pots/ui.pots";
+
+export default [
+User,
+Pots,
+]
---
+++ apps/client/entities/pots/ui.pots.ts
@@ -0,0 +1,15 @@
+
+import show from "./show.pots";
+import create from "./create.pots";
+import edit from "./edit.pots";
+import list from "./list.pots";
+
+export default { 
+  name: "pots",
+  label: "pots",
+  hide: false,
+  create,
+  edit,
+  list,
+  show,
+}
